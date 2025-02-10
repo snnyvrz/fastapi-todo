a77348f9ba1fcd450ec26a9e1c46c8551be95908
sinaniya <sinaniya@gmail.com>
2025-02-10 02:41:26 +0100

    updated tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,29 +1,59 @@
 from fastapi.testclient import TestClient
 
 
-def test_root(client: TestClient):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "Sina"}
-
-
-def test_signup(client: TestClient):
+def get_token(client: TestClient) -> str:
     response = client.post(
-        "/signup/",
-        json={
+        "/auth/signup/",
+        data={
             "username": "sinaniya",
             "password": "12345678",
-            "confirm_password": "12345678",
         },
+    )
+
+    if response.status_code == 409:
+        response = client.post(
+            "/auth/signin/",
+            data={
+                "username": "sinaniya",
+                "password": "12345678",
+            },
+        )
+    assert response.status_code == 200
+    return response.json()["access_token"]
+
+
+def test_me(client: TestClient):
+    response = client.get("/auth/me/")
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
+
+
+def test_me_authenticated(client: TestClient):
+    access_token = get_token(client)
+    response = client.get(
+        "/auth/me/", headers={"Authorization": f"Bearer {access_token}"}
     )
     assert response.status_code == 200
     assert response.json()["username"] == "sinaniya"
 
 
+def test_signup(client: TestClient):
+    response = client.post(
+        "/auth/signup/",
+        data={
+            "username": "sinaniya2",
+            "password": "12345678",
+            "confirm_password": "12345678",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json().get("access_token") is not None
+
+
 def test_signup_existing_user(client: TestClient):
     response = client.post(
-        "/signup/",
-        json={
+        "/auth/signup/",
+        data={
             "username": "sinaniya",
             "password": "12345678",
             "confirm_password": "12345678",
@@ -35,20 +65,20 @@ def test_signup_existing_user(client: TestClient):
 
 def test_user_signin(client: TestClient):
     response = client.post(
-        "/signin/",
-        json={
+        "/auth/signin/",
+        data={
             "username": "sinaniya",
             "password": "12345678",
         },
     )
     assert response.status_code == 200
-    assert response.json()["username"] == "sinaniya"
+    assert response.json().get("access_token") is not None
 
 
 def test_user_signin_not_found(client: TestClient):
     response = client.post(
-        "/signin/",
-        json={
+        "/auth/signin/",
+        data={
             "username": "sina",
             "password": "12345678",
         },
@@ -59,8 +89,8 @@ def test_user_signin_not_found(client: TestClient):
 
 def test_user_signin_wrong_password(client: TestClient):
     response = client.post(
-        "/signin/",
-        json={
+        "/auth/signin/",
+        data={
             "username": "sinaniya",
             "password": "123456789",
         },
